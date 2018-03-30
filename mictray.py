dd89457770e2f82cded9354ee5fc829be03f66d8
Karl Swanson <karlcswanson@gmail.com>
2018-03-29 20:53:56 -0500

    Added thread for colorupdate

--- mictray.py
+++ mictray.py
@@ -27,17 +27,22 @@ class Slot:
         if parts[0][:2] == 'HH' or parts[0][:2] == 'BP':
             tx_id = parts[0]
             name = ' '.join(parts[1:])
-            oled.drawName(disp,tx_id,name)
+            # JUST FOR THE DEMO
+            if self.number == 1:
+                oled.drawName(disp,tx_id,name)
         else:
-            oled.drawName(disp,'',self.tx.chan_name)
+            if self.number == 1:
+                oled.drawName(disp,'',self.tx.chan_name)
+
 
     def update_state(self, state):
         self.state = state
+        led.set_strip(strip,self.number,self.state)
 
     def update(self):
         state = self.tx.tx_state()
         if self.chan_name != self.tx.chan_name:
-            self.update_name(self.tx.chan_name, disp)
+            self.update_name()
         if self.state != state:
             self.update_state(state)
 
@@ -47,12 +52,18 @@ def slot_setup():
         for tx in rx.transmitters:
             slots.append(Slot(tx.slot,tx.chan_name, tx))
 
+    for slot in slots:
+        slot.update_name()
+        slot.update_state(slot.tx.tx_state())
+    strip.show()
+
 
 def update_slots():
     while True:
         for slot in slots:
             slot.update()
         time.sleep(.5)
+        strip.show()
 
 
 def main():
@@ -60,16 +71,18 @@ def main():
     slot_setup()
 
 
-    # t1 = threading.Thread(target=shure.WirelessPoll)
-    # t2 = threading.Thread(target=shure.WirelessListen)
+    t1 = threading.Thread(target=shure.WirelessPoll)
+    t2 = threading.Thread(target=shure.WirelessListen)
+    t3 = threading.Thread(target=update_slots)
 
     #
-    # t1.start()
-    # t2.start()
+    t1.start()
+    t2.start()
+    t3.start()
 
-    # while True:
-    time.sleep(3)
-    shure.print_ALL()
+    while True:
+        time.sleep(3)
+        shure.print_ALL()
 
 
 
