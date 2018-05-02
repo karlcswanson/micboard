841a540e1abd4021fe1839e4d8341343082efcb0
Karl Swanson <karlcswanson@gmail.com>
2018-05-02 15:45:52 -0500

    multi-screen support, demo data mode

--- static/script.js
+++ static/script.js
@@ -1,15 +1,49 @@
 var prefix = "HH"
+var dataURL = '/data';
+
 $(document).ready(function() {
-  setInterval(loadJson, 500);
+  start_slot = getUrlParameter('start_slot');
+  stop_slot = getUrlParameter('stop_slot');
+  demo = getUrlParameter('demo');
+  console.log('Start: ' + start_slot);
+  console.log('Stop:  ' + stop_slot);
+  if (demo == 'true') {
+    dataURL = '/static/data.json';
+    loadJson();
+  }
+  else {
+    setInterval(loadJson, 500);
+  }
 });
+
+// https://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
 
 function loadJson(){
   transmitters = [];
-  $.getJSON( "/data", function( data ) {
+
+  $.getJSON( dataURL, function( data ) {
       for(i in data.receivers){
           for (j in data.receivers[i].tx){
             transmitters.push(data.receivers[i].tx[j]);
           }
+      }
+      if (start_slot && stop_slot){
+        transmitters = transmitters.slice(start_slot-1,stop_slot);
       }
       transmitters.sort(function(a, b){return a.slot - b.slot});
       initialMap();
