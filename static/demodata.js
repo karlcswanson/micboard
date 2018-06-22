bccaba7c9beb8e85468cef1ccdd0a9340e2465cf
Karl Swanson <karlcswanson@gmail.com>
2018-06-22 13:10:32 -0500

    Live Data!

---
+++ static/demodata.js
@@ -0,0 +1,112 @@
+var batterySample = {
+  0:{
+      battery: 255,
+      status: ['CRITICAL','UNASSIGNED']
+    },
+  1:{
+      battery: 1,
+      status: ['CRITICAL']
+    },
+  2:{
+      battery: 2,
+      status: ['CRITICAL']
+    },
+  3:{
+      battery: 3,
+      status: ['REPLACE','PREV_REPLACE']
+    },
+  4:{
+      battery: 4,
+      status: ['GOOD','PREV_GOOD','UNASSIGNED']
+    },
+  5:{
+      battery: 5,
+      status: ['GOOD','PREV_GOOD']
+    }
+}
+
+var rfSample = ['AX','XB','XX'];
+
+
+var name_sample = ['Fatai','Marshall','Delwin','Amaris','Tracy TB','Backup',
+                   'Steve','Heather','JE','Sharon','Clary','Bob','Del ACU',
+                   'Matt','Matt ACU','Matt Sax','Karl','Jordan','Josue',
+                   'Hallie','Rebekah','Dan','Stephen','Max','Tom','Nick',''];
+
+
+// https://gist.github.com/kerimdzhanov/7529623
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomNameGenerator(){
+  var len = name_sample.length;
+  var index = getRandomInt(0,len-1);
+  return name_sample[index];
+}
+
+function randomRfSampleGenerator() {
+  return rfSample[getRandomInt(0,2)];
+}
+
+function randomAudioGenerator(){
+  return getRandomInt(0,50);
+}
+
+function randomRfGenerator(){
+  return getRandomInt(0,115);
+}
+
+function randomBatteryGenerator() {
+  var batt_index = getRandomInt(0,5);
+  var battery = batterySample[batt_index];
+  var len = battery.status.length;
+  var status_index = getRandomInt(0,len-1);
+
+  var res = {
+              battery: battery.battery,
+              status: battery.status[status_index]
+            }
+  return res;
+}
+
+function randomDataGenerator(){
+  var slots = Object.keys(transmitters).map(Number);
+  var min = Math.min.apply(Math, slots);
+  var max = Math.max.apply(Math, slots);
+  var slot = getRandomInt(min, max);
+
+  var battery = randomBatteryGenerator();
+
+
+
+  var res = {
+    "name": randomNameGenerator(),
+    "antenna": randomRfSampleGenerator(),
+    "audio_level": randomAudioGenerator(),
+    "rf_level": randomRfGenerator(),
+    "slot": slot,
+    "battery": battery.battery,
+    "status": battery.status
+  }
+  return res;
+
+}
+
+function randomCharts(){
+  var slots = Object.keys(transmitters).map(Number);
+  slots.forEach(function(slot){
+    charts[slot].audioSeries.append(Date.now(), randomRfGenerator());
+    charts[slot].rfSeries.append(Date.now(), randomAudioGenerator());
+    updateDiversity(randomDataGenerator());
+  })
+}
+
+function autoUpdateNames(){
+    updateSlot(randomDataGenerator());
+}
+
+function autoRandom(){
+  setInterval(autoUpdateNames,500);
+  setInterval(randomCharts,125);
+}
--- static/script.js
+++ static/script.js
@@ -1,20 +1,43 @@
 var prefix = "HH"
 var dataURL = '/data';
+var transmitters = {};
+var charts = {};
 
 $(document).ready(function() {
   start_slot = getUrlParameter('start_slot');
   stop_slot = getUrlParameter('stop_slot');
   demo = getUrlParameter('demo');
-  console.log('Start: ' + start_slot);
-  console.log('Stop:  ' + stop_slot);
   if (demo == 'true') {
     dataURL = 'static/data.json';
-    loadJson();
+    initialMap();
   }
   else {
-    setInterval(loadJson, 500);
+    initialMap();
+    setInterval(JsonUpdate, 500);
+    // wsConnect();
   }
+
 });
+
+
+function wsConnect(){
+  var loc = window.location, new_uri;
+  if (loc.protocol === "https:") {
+    new_uri = "wss:";
+  } else {
+    new_uri = "ws:";
+  }
+  new_uri += "//" + loc.host;
+  new_uri +=  "/ws";
+  var socket = new WebSocket(new_uri);
+
+  socket.onmessage = function(msg){
+    mic_data = JSON.parse(msg.data);
+    updateSlot(mic_data);
+
+  }
+}
+
 
 // https://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
 var getUrlParameter = function getUrlParameter(sParam) {
@@ -32,32 +55,164 @@ var getUrlParameter = function getUrlParameter(sParam) {
     }
 };
 
-
-function loadJson(){
-  transmitters = [];
-
+function JsonUpdate(){
   $.getJSON( dataURL, function( data ) {
-      for(i in data.receivers){
-          for (j in data.receivers[i].tx){
-            transmitters.push(data.receivers[i].tx[j]);
-          }
+    for(i in data.receivers) {
+      for (j in data.receivers[i].tx) {
+        updateSlot(data.receivers[i].tx[j]);
       }
-      if (start_slot && stop_slot){
-        transmitters = transmitters.slice(start_slot-1,stop_slot);
-      }
-      transmitters.sort(function(a, b){return a.slot - b.slot});
-      initialMap();
+    }
   });
 }
 
-function initialMap(){
-  $(".above-mid").text("");
-  for(i in transmitters){
-    var insert = '<div id="slot-' + transmitters[i].slot + '" class="col-sm ' +
-                                    transmitters[i].status + '">\n' +
-                 '<p class="mic_id">'+ prefix + ("0" + transmitters[i].slot).slice(-2) + '</p>\n' +
-                 '<p class="name">' + transmitters[i].name + '</p></div>';
-
-    $(".above-mid").append(insert);
+function updateSlot(data){
+  if (start_slot && stop_slot){
+    if (start_slot <= data.slot && data.slot <= stop_slot) {
+      transmitters[data.slot] = data;
+    }
   }
+  else {
+    transmitters[data.slot] = data;
+  }
+
+  var slot = "slot-" + data.slot;
+  var t = document.getElementById(slot);
+  t.querySelector('div.mic_name').className = 'mic_name';
+  t.querySelector('div.mic_name').classList.add(data.status);
+  t.querySelector('p.name').innerHTML = data.name;
+  t.querySelector('p.mic_id').innerHTML = prefix + ("0" + data.slot).slice(-2);
+  updateBattery(data);
+  updateDiversity(data);
+  charts[data.slot].audioSeries.append(Date.now(), data.audio_level);
+  charts[data.slot].rfSeries.append(Date.now(), data.rf_level);
+}
+
+
+var BatteryTable = {
+  '0':  ['batt_led_off', 'batt_led_off', 'batt_led_off', 'batt_led_off', 'batt_led_off'],
+  '1':  ['batt_led_danger', 'batt_led_off', 'batt_led_off', 'batt_led_off', 'batt_led_off'],
+  '2':  ['batt_led_danger', 'batt_led_danger', 'batt_led_off', 'batt_led_off', 'batt_led_off'],
+  '3':  ['batt_led_warning', 'batt_led_warning', 'batt_led_warning', 'batt_led_off', 'batt_led_off'],
+  '4':  ['batt_led_good', 'batt_led_good', 'batt_led_good', 'batt_led_good', 'batt_led_off'],
+  '5':  ['batt_led_good', 'batt_led_good', 'batt_led_good', 'batt_led_good', 'batt_led_good'],
+  '255':['batt_led_off', 'batt_led_off', 'batt_led_off', 'batt_led_off', 'batt_led_off'],
+  'led':[]
+}
+
+function updateBattery(data){
+  var slot = "slot-" + data.slot;
+  var t = document.getElementById(slot);
+  var outputBars = BatteryTable[data.battery];
+
+  t.querySelectorAll('.battery-bar').forEach(function (data) {
+    data.classList.remove('batt_led_off', 'batt_led_danger','batt_led_warning','batt_led_good');
+  });
+
+  t.querySelector('.battery-bar-1').classList.add(outputBars[0]);
+  t.querySelector('.battery-bar-2').classList.add(outputBars[1]);
+  t.querySelector('.battery-bar-3').classList.add(outputBars[2]);
+  t.querySelector('.battery-bar-4').classList.add(outputBars[3]);
+  t.querySelector('.battery-bar-5').classList.add(outputBars[4]);
+
+}
+
+var diversityTable = {
+  'AX': ['diversity-bar-on','diversity-bar-off'],
+  'XB': ['diversity-bar-off','diversity-bar-on'],
+  'XX': ['diversity-bar-off','diversity-bar-off']
+}
+
+function updateDiversity(data){
+  var slot = "slot-" + data.slot;
+  var t = document.getElementById(slot);
+  var outputBars = diversityTable[data.antenna];
+
+  t.querySelectorAll('.diversity-bar').forEach(function (data) {
+    data.classList.remove('diversity-bar-on','diversity-bar-off');
+  });
+
+  t.querySelector('.diversity-bar-1').classList.add(outputBars[0]);
+  t.querySelector('.diversity-bar-2').classList.add(outputBars[1]);
+}
+
+function dataFilter(data){
+  for(i in data.receivers){
+    for (j in data.receivers[i].tx){
+      var tx = data.receivers[i].tx[j];
+      if (start_slot && stop_slot){
+        if (start_slot <= tx.slot && tx.slot <= stop_slot) {
+          transmitters[tx.slot] = tx;
+        }
+      }
+      else {
+        transmitters[tx.slot] = tx;
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+function initialMap() {
+  $.getJSON( dataURL, function(data) {
+    dataFilter(data);
+    $(".above-mid").text("");
+    var tx = transmitters;
+    for(i in tx) {
+      var t = document.getElementById("column-template").content.cloneNode(true);
+      t.querySelector('div.col-sm').id = 'slot-' + tx[i].slot;
+      t.querySelector('div.mic_name').classList.add(tx[i].status);
+      t.querySelector('p.mic_id').innerHTML = prefix + ("0" + tx[i].slot).slice(-2);
+      t.querySelector('p.name').innerHTML = tx[i].name;
+      document.getElementById('micboard').appendChild(t);
+      charts[tx[i].slot] = initChart('slot-' + tx[i].slot);
+    }
+  });
+}
+
+
+function initChart(chartID) {
+  var chart = {};
+  chart.audioSeries = new TimeSeries();
+  chart.rfSeries = new TimeSeries();
+
+  var audioCanvas = document.getElementById(chartID).querySelector('canvas.audio-graph');
+  var rfCanvas = document.getElementById(chartID).querySelector('canvas.rf-graph');
+
+  var chartOptions = {
+    responsive:true,
+    // interpolation:'step',
+    millisPerPixel: 25,
+    grid: {
+      verticalSections:0,
+      strokeStyle:'#000000'
+    },
+    labels:{
+      disabled:true
+    },
+    maxValue:115,
+    minValue:0
+  };
+  var audioChart = new SmoothieChart(chartOptions);
+  var rfChart = new SmoothieChart(chartOptions);
+
+  audioChart.addTimeSeries(chart.audioSeries, {
+    strokeStyle: 'rgba(0, 255, 0, 1)',
+    fillStyle: 'rgba(0, 255, 0, 0.2)',
+    lineWidth: 4
+  });
+
+  rfChart.addTimeSeries(chart.rfSeries, {
+    strokeStyle: 'rgba(255, 0, 0, 1)',
+    fillStyle: 'rgba(255, 0, 0, 0.2)',
+    lineWidth: 4
+  });
+
+  audioChart.streamTo(audioCanvas, 100);
+  rfChart.streamTo(rfCanvas, 100);
+  return chart;
 }
