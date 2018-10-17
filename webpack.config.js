5ec66a052f5f4765b9e88182790bdbea9f1deb88
Karl Swanson <karlcswanson@gmail.com>
2018-10-16 22:18:59 -0500

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
           loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'fonts/'
+              outputPath: 'fonts/',
+              publicPath: 'static/fonts/'
             }
         }]
       },
