9fedd04f8975b005dc4dd75a156d0f13ad11ea41
Karl Swanson <karlcswanson@gmail.com>
2018-11-10 15:54:25 -0600

    Electron cleanup

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ app.on('ready', () => {
     {type: 'separator'},
     {role: 'quit'}
   ])
-  tray.setToolTip('This is my application.')
+  tray.setToolTip('micboard')
   tray.setContextMenu(contextMenu)
 })
 
@@ -78,27 +78,3 @@ app.on('ready', () => {
 
 app.on('ready', createPyProc)
 app.on('will-quit', exitPyProc)
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-// app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
