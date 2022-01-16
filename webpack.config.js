9ec6e2927e4546a3d8f64f2bd9ff82952ca46d61
Karl Swanson <karlcswanson@gmail.com>
2022-01-15 21:47:41 -0500

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -48,15 +48,6 @@ module.exports = {
           filename: 'fonts/[name][ext]',
         }
       },
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          // presets: ['env', 'react']
-        },
-      },
     ],
   },
 };
