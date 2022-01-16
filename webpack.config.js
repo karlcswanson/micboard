3b8d36c8167fe71b4e34d4f1887706ea0a1e42a8
Karl Swanson <karlcswanson@gmail.com>
2022-01-15 21:06:40 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -31,20 +31,15 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.scss$/,
-        use: [{
-          loader: 'style-loader',
-        }, {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
-          },
-        }, {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true,
-          },
-        }],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
